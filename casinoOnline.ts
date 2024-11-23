d69e195b610ee3a7c074743c916caaa98d717001
Ignacio <nachitogalardo@hotmail.com.ar>
2024-11-22 22:09:49 -0300

    Prueba apuesta

--- casinoOnline.ts
+++ casinoOnline.ts
@@ -1,40 +1,19 @@
 import { Usuario } from "./usuario";
 import { Maquina } from "./maquina";
 
-export class CasinoOnline {
-    public usuarios : Usuario [];
+export class CasinoOnline  {
     public maquinas : Maquina [];
 
     constructor () {
-
+        this.maquinas = [];
     }
 
-    public crearUsuario (usuarioNuevo : Usuario){
-        this.usuarios.push (usuarioNuevo);
-
+    public crearUsuario (nombre:string):Usuario{
+        let usuario:Usuario = new Usuario(nombre);
+        return usuario;
     }
 
-    public eliminarUsuario (usuarioAEliminar : Usuario){
-        if (usuarioAEliminar != undefined && this.usuarios.includes(usuarioAEliminar)) {
-            const iUsuario : number = this.usuarios.indexOf (usuarioAEliminar);
-            this.usuarios.slice (iUsuario, 1);
-        } else {
-            console.log ("El usuario NO existe en la base de datos");
-        }
-    }
-//buscar la forma para usar el setter de usuario
-    public setModificarUsuario (usuarioAModificar : Usuario) {
-        if (usuarioAModificar != undefined && this.usuarios.includes(usuarioAModificar)) {
-            let usuario = this.usuarios.indexOf (usuarioAModificar);
-            this.usuarios.slice (usuario);
-            this.usuarios.push (usuarioAModificar);
-        } else {
-            console.log ("El usuario NO existe en la base de datos");
-        }
-    }
-
-    public cargarSaldo (saldoACargar:number) : number {
-        saldoACargar = usuarios.saldo + saldoACargar;
-        return saldoACargar;
+    public getmaquinas():Maquina[] {
+       return this.maquinas;
     }
 }
--- maquina.ts
+++ maquina.ts
@@ -1,10 +1,13 @@
-export abstract class Maquina {
+import * as rls from "readline-sync";
+import { Usuario } from "./usuario";
+
+export  class Maquina {
     public apuestaMinima : number;
     public pozoAcumulativo : number;
     
-    constructor (apuestaMinima : number, pozoAcumulativo : number) {
-        this.apuestaMinima = apuestaMinima;
-        this.pozoAcumulativo = pozoAcumulativo;
+    constructor () {
+        this.apuestaMinima = 50;
+        this.pozoAcumulativo = 1000;
     }
 
     public getApuestaMinima(): number {
@@ -20,6 +23,21 @@ export abstract class Maquina {
         this.pozoAcumulativo = pozoAcumulativo;
     }
 
-    public abstract apostar () ;
+    public apostar (usuario:Usuario) :number {
+        let apuestaUsuario = rls.questionInt("Ingrese 1 para apostar 50 creditos: \nIngrese 2 para apostar 100 creditos: \nIngrese 3 para apostar 200 creditos: ");
+        if (apuestaUsuario==1){usuario.setSaldo(-50)}
+        else if (apuestaUsuario==2){usuario.setSaldo(-100)}
+        else if (apuestaUsuario==3){usuario.setSaldo(-200)}
+        return usuario.getSaldo();
+    };
 
 }
+
+let maquina = new Maquina ();
+let usuario = new Usuario ("Ignacio");
+let apuesta = maquina.apostar (usuario);
+
+usuario.setSaldo(200);
+let resultado = usuario.saldo+apuesta;
+console.log(resultado);
+
